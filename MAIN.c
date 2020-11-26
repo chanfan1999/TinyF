0a5aeb11a68a92356a52375af33f4579eb41a5b6
wumcpq <wumqing@yeah.net>
2020-11-26 11:36:36 +0800

    将语法树输出重定义到文件

--- MAIN.c
+++ MAIN.c
@@ -39,7 +39,7 @@ FILE * code;
 /* allocate and set tracing flags */
 int EchoSource = FALSE;
 int TraceScan = FALSE;
-int TraceParse = FALSE;
+int TraceParse = TRUE;
 int TraceAnalyze = FALSE;
 int TraceCode = FALSE;
 
@@ -57,16 +57,18 @@ main( int argc, char * argv[] )
      strcat(pgm,".tny");
   source = fopen(pgm,"r");
   if (source==NULL)
-  { fprintf(stderr,"File %s not found\n",pgm);
+  { printf(stderr,"File %s not found\n",pgm);
     exit(1);
   }
-  listing = stdout; /* send listing to screen */
-  fprintf(listing,"\nTINY COMPILATION: %s\n",pgm);
+  // listing = stdout; /* send listing to screen */
+  listing = fopen("syntaxTree.txt","w"); // output to txt File
+  printf("\nTINY COMPILATION: %s\n",pgm);
 #if NO_PARSE
   while (getToken()!=ENDFILE);
 #else
   syntaxTree = parse();
   if (TraceParse) {
+    printf("checkout in file:syntaxTree.txt!");
     fprintf(listing,"\nSyntax tree:\n");
     printTree(syntaxTree);
   }
